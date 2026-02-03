ea942cbf15d237f3263d0c002027b6425f3f81ab
somilsingh1 <somilstudies@gmail.com>
2026-02-03 21:21:15 +0530

    Changes to be committed: 	new file:   insert_sort 	new file:   rev5.cpp

---
+++ revision/rev5.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v1;
+    vector<int> v2(3,5);
+    for(auto x : v2)
+        cout<<x<<endl;
+
+cout<<"vector v3 : \n";
+    
+    vector <int> v3 = {1,4,6,7,};
+    for(auto x : v3)
+        cout<<x<<endl;
+    
+    
+}
